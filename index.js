a21d141c2559d2e6b2cc216d3f5fe57e30d1ed18
aj07256768-sys <aj07256768@gmail.com>
2026-04-14 12:29:28 +0300

    completed the task

--- index.js
+++ index.js
@@ -12,3 +12,65 @@ module.exports = {
   ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
   ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
 };
+
+
+ // object attendee
+
+ const attendee={ 
+
+    attendeeId:"T001",
+    namee:"Alice Smith",
+    evenT: "JavaScript Conference",
+    ticketType: "VIP",
+    ticketPrice: 150.0
+ }
+
+
+  // logAttendeeName
+
+function logAttendeeName(attendee){
+
+ return attendee.namee;
+
+
+ }
+     // ticket price 
+
+  const logTicketPrice=(attendee)=>{
+    return attendee.ticketPrice;
+
+  }
+
+// updating ticket type 
+  const updateTicketType =(attendee,newtickettype)=>{
+
+    return attendee.ticketType='supervip';
+
+  }
+ 
+// updating ticket price in an meeting attendee
+  const updateTicketPrice=(attendee,newticketprice)=>{
+
+    return attendee.ticketPrice= 500;
+
+  } 
+
+
+  // removeEventProperty  ( unfished)
+  const removeEventProperty=(attendee,event)=>{
+     
+    delete attendee.evenT;
+    return attendee
+
+         
+  }
+
+
+    const addCheckedInProperty=(attendee,checkedIn )=>{
+
+     return attendee.checkedIn ='true'
+
+    }
+     
+      console.log(addCheckedInProperty(attendee)) 
+
